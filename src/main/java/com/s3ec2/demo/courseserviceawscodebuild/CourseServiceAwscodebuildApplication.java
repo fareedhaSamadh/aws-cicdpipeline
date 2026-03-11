725c1eb146fbcc2145dcc4689cc8ae147740a99c
fareedha <fareedhasamadh24@gmail.com>
2026-03-11 14:04:34 -0700

    docker file and buildspec.yaml

---
+++ src/main/java/com/s3ec2/demo/courseserviceawscodebuild/CourseServiceAwscodebuildApplication.java
@@ -0,0 +1,13 @@
+package com.s3ec2.demo.courseserviceawscodebuild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseServiceAwscodebuildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseServiceAwscodebuildApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/s3ec2/demo/courseserviceawscodebuild/CourseServiceAwscodebuildApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s3ec2.demo.courseserviceawscodebuild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseServiceAwscodebuildApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
